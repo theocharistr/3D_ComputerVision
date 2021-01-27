bd2de63bff09c5d1b55b09ef183038bf9a79b7f9
Theocharis Triantafyllidis <theocharistr@gmail.com>
2021-01-27 15:10:01 +0100

    Update FV.cpp

--- Affine Transformation/src/FV.cpp
+++ Affine Transformation/src/FV.cpp
@@ -1,11 +1,11 @@
 /*
-The project for the third lab visualizes a 3D point cloud. The task is to improve the original projects:
+The project visualizes a 3D point cloud. The task is to improve the original projects:
 - Add mouse-controlling to the project, similarly to Meshlab. The object is rotated if the left mouse button 
 is pressed and the mouse position is changed (dragged). Vertical and horizontal movement changes the angles 
-u and v, respectively. (3%)
+u and v, respectively.  
 - Animate the object, independently to the user's control. Rotate the objects with a constant rotation w.r.t 
 time, all the three principal axes should be used for the rotation (2%), periodically scale the object between
-scale factors 0.5 and 2.0 (3%)
+scale factors 0.5 and 2.0  
 */
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
